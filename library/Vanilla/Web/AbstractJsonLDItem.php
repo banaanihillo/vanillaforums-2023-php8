d91b28a01db6ea2c79f61b46408d91f8dd86044a
banaanihillo <valtteri.murtonen@puheet.com>
2023-02-02 11:27:08 +0200

    JSON serialize to return mixed or return type will change

---
+++ library/Vanilla/Web/AbstractJsonLDItem.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author Adam Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+namespace Vanilla\Web;
+
+use Garden\Web\Data;
+
+/**
+ * Item representing some JSON-LD item.
+ */
+abstract class AbstractJsonLDItem implements \JsonSerializable
+{
+    /**
+     * Calculate the value of the JSON-LD item.
+     *
+     * @return Data
+     */
+    abstract public function calculateValue(): Data;
+
+    /**
+     * @inheritdoc
+     */
+    public function jsonSerialize(): mixed
+    {
+        return $this->calculateValue();
+    }
+}
