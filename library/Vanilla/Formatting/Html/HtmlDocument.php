c8441d51eb9ef9a3ccd7419e327d038411d135ac
banaanihillo <valtteri.murtonen@puheet.com>
2023-02-17 10:29:15 +0200

    Quote the console log statement instead

--- library/Vanilla/Formatting/Html/HtmlDocument.php
+++ library/Vanilla/Formatting/Html/HtmlDocument.php
@@ -100,8 +100,8 @@ class HtmlDocument implements TextDOMInterface
         $xmlErrors = libxml_get_errors();
         if (sizeof($xmlErrors) > 0) {
             prettyPrint($xmlErrors);
-            prettyPrint("'" . $innerHtml . "'");
-            echo "<script>console.log($innerHtml)</script>";
+            prettyPrint($innerHtml);
+            echo "<script>console.log('\'' + $innerHtml + '\'')</script>";
         }
         unset($xmlErrors);
         libxml_clear_errors();
