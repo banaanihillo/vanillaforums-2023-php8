1c57dbd335e1ba16bcc3ebfd0bb0a5f51bd0490b
banaanihillo <valtteri.murtonen@puheet.com>
2023-02-17 10:25:08 +0200

    Single quote the raw HTML

--- library/Vanilla/Formatting/Html/HtmlDocument.php
+++ library/Vanilla/Formatting/Html/HtmlDocument.php
@@ -100,7 +100,7 @@ class HtmlDocument implements TextDOMInterface
         $xmlErrors = libxml_get_errors();
         if (sizeof($xmlErrors) > 0) {
             prettyPrint($xmlErrors);
-            prettyPrint($innerHtml);
+            prettyPrint("'" + $innerHtml + "'");
             echo "<script>console.log($innerHtml)</script>";
         }
         unset($xmlErrors);
