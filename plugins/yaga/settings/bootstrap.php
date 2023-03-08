9709b46d21f4ff5807bde23b51f0afc2684e3384
banaanihillo <valtteri.murtonen@puheet.com>
2023-03-08 12:59:52 +0200

    Path applications changed to path plugins

--- plugins/yaga/settings/bootstrap.php
+++ plugins/yaga/settings/bootstrap.php
@@ -3,9 +3,17 @@
 // Register Yaga library classes and interfaces in the autoloader
 $Map     = Gdn_Autoloader::MAP_LIBRARY;
 $Context = Gdn_Autoloader::CONTEXT_APPLICATION;
-$Path    = PATH_APPLICATIONS . DS . 'yaga' . DS . 'library';
+$Path    = PATH_PLUGINS . DS . 'yaga' . DS . 'library';
 $Options = array('Extension' => 'yaga');
 Gdn_Autoloader::start();
 Gdn_Autoloader::RegisterMap($Map, $Context, $Path, $Options);
 
-require_once(PATH_APPLICATIONS . DS . 'yaga' . DS . 'library' . DS . 'functions.render.php');
+require_once(
+    PATH_PLUGINS
+    . DS
+    . 'yaga'
+    . DS
+    . 'library'
+    . DS
+    . 'functions.render.php'
+);
