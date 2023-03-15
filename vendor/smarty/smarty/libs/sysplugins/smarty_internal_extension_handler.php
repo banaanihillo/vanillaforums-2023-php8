0873cdd49db560d26b452133b15dc027aef8d2b3
banaanihillo <valtteri.murtonen@puheet.com>
2023-03-15 12:45:41 +0200

    Declare the static properties mentioned in the docstring

--- vendor/smarty/smarty/libs/sysplugins/smarty_internal_extension_handler.php
+++ vendor/smarty/smarty/libs/sysplugins/smarty_internal_extension_handler.php
@@ -61,14 +61,18 @@ class Smarty_Internal_Extension_Handler
     private $resolvedProperties = array();
 
     /** Added for PHP8.2+ support */
+    /** Runtime extensions */
+    private $_codeFrame;
+    private $_foreach;
+    private $_writeFile;
+    /** Method extensions */
+    public $getTemplateVars;
+    public $registerPlugin;
+    public $loadPlugin;
+    /** Other */
     // public $setDebugTemplate;
     // public $setLiterals;
-    // public $_codeFrame;
-    // public $_foreach;
-    // public $getTemplateVars;
-    // public $getLiterals;
-    // public $_writeFile;
-    // public $registerPlugin;
+    // public $getLiterals; // Why is that source control thing there
     // public $addLiterals;
 
     /**
