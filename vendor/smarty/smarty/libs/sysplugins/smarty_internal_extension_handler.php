8c1113a7884ec714c863be0360ac90772d8ec58d
banaanihillo <valtteri.murtonen@puheet.com>
2023-03-15 13:41:03 +0200

    Attempt to declare static class properties

--- vendor/smarty/smarty/libs/sysplugins/smarty_internal_extension_handler.php
+++ vendor/smarty/smarty/libs/sysplugins/smarty_internal_extension_handler.php
@@ -79,6 +79,13 @@ class Smarty_Internal_Extension_Handler
     // // public $getLiterals; // Why is that source control thing there
     // // public $addLiterals;
 
+    public Smarty_Internal_Method_SetDebugTemplate $setDebugTemplate;
+    public Smarty_Internal_Method_RegisterPlugin $registerPlugin;
+    public Smarty_Internal_Runtime_CodeFrame $_codeFrame;
+    public Smarty_Internal_Method_Literals $getLiterals;
+    public Smarty_Internal_Method_Literals $addLiterals;
+    public Smarty_Internal_Method_Literals $setLiterals;
+
     /**
      * Call external Method
      *
